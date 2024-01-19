b563cb9409a07d395418b245ead11846e73f3eba
Greg Coffeng <gcoffeng@gmail.com>
2024-01-19 14:19:28 -0800

    out of time commit

--- src/cosmos.js
+++ src/cosmos.js
@@ -5,7 +5,7 @@ const shuttleUtil = require('./util/shuttle')
 const cadet = require('./cadet')
 
 const listen = async () => {
-  const shuttleDb = initShuttleDb()
+  const shuttleDb = await initShuttleDb()
   const spaceTravelEmitter = createSpaceTravelEmitter()
   let totalCrewCount = 0
   spaceTravelEmitter.on('space-request', evt => {
@@ -23,30 +23,49 @@ const listen = async () => {
         'no more space requests, exiting.',
         `db can be viewed: ${shuttleDb.getDbFilename()}`
       ].join(' ')
-    )
-  })
-}
-
-const onSpaceTravelRequested = async ({ shuttleDb, cosmonautId }) => {
-  const shuttles = await shuttleDb.read()
-  const availableShuttle = shuttles
-    .find(({ date, capacity }) => date >= 0 && capacity > 0)
-  if (!availableShuttle) {
-    throw new Error(
-      `unable to schedule cosmonautId ${cosmonautId}, no shuttles available`
-    )
+      )
+    })
   }
-  log(
-    `found shuttle for cosmonautId ${cosmonautId}, shuttle ${
-      availableShuttle.name
-    }`
-  )
-  --availableShuttle.remainingCapacity
-  availableShuttle.crew.push(cosmonautId)
-  await shuttleDb.write(availableShuttle.name, availableShuttle)
-  await cadet.logWelcomeLetter({ cosmonautId, shuttle: availableShuttle })
-}
+  
+  // [
+  //'{"date":10,"name":"discovery","crew":[],"remainingCapacity":1}',
+  //    '{"date":3,"name":"sputnik-2","crew":[],"remainingCapacity":3}',
+  //    '{"date":-5,"name":"tom-hanks","crew":[],"remainingCapacity":0}'
+  // ]
+  const onSpaceTravelRequested = async ({ shuttleDb, cosmonautId }) => {
+    const shuttles = await shuttleDb.read()
+    // console.log(shuttles)
+    const availableShuttleData = Object.values(shuttles);
+    console.log("availableShuttleData", availableShuttleData);
 
-module.exports = {
-  listen
-}
+   const availableShuttleParsed = availableShuttleData.map(x => {
+    if (typeof x === "string") return JSON.parse(x);
+    else return x;
+   });
+   console.log(availableShuttleParsed)
+    const availableShuttle = availableShuttleParsed
+    .find(({ date, remainingCapacity }) => {
+
+      if (date >= 0 && remainingCapacity > 0) return true;
+      else return false;
+    })
+    if (!availableShuttle) {
+      throw new Error(
+        `unable to schedule cosmonautId ${cosmonautId}, no shuttles available`
+        )
+      }
+      log(
+        `found shuttle for cosmonautId ${cosmonautId}, shuttle ${
+          availableShuttle.name
+        }`
+        )
+        --availableShuttle.remainingCapacity
+        availableShuttle.crew.push(cosmonautId)
+        await shuttleDb.write(availableShuttle.name, availableShuttle)
+        await cadet.logWelcomeLetter({ cosmonautId, shuttle: availableShuttle })
+      }
+      
+      module.exports = {
+        listen
+      }
+      
